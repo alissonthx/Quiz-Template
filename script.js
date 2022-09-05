e7e62e951739968e1ac83fddea768241dbccb957
alissonthx <alissonthx208@gmail.com>
2022-09-04 22:09:48 -0300

    add: coins sound function

--- script.js
+++ script.js
@@ -1,118 +1,124 @@
 const quizData = [
   {
     question: "What's the name of the Goat's sin of Lust",
-    a: 'Gowther',
-    b: 'Rasuto',
-    c: 'Ban',
-    d: 'Armor Giant',
-    correct: 'a'
+    a: "Gowther",
+    b: "Rasuto",
+    c: "Ban",
+    d: "Armor Giant",
+    correct: "a",
   },
   {
     question: "What's the name of Ban's partner",
-    a: 'Fairy',
-    b: 'Rasuto',
-    c: 'Elaine',
-    d: 'Fox',
-    correct: 'c'
+    a: "Fairy",
+    b: "Rasuto",
+    c: "Elaine",
+    d: "Fox",
+    correct: "c",
   },
   {
     question: "What's the sacred tresure of King in Seven deadly sins ",
-    a: 'Chastiefol',
-    b: 'Grizzly',
-    c: 'Grizzly Bear',
-    d: 'King Treasure',
-    correct: 'a'
+    a: "Chastiefol",
+    b: "Grizzly",
+    c: "Grizzly Bear",
+    d: "King Treasure",
+    correct: "a",
   },
   {
     question: "What's the sacred tresure of Escanor in Seven deadly sins",
-    a: 'Gowther',
-    b: 'Rasuto',
-    c: 'Ritha',
-    d: 'Armor Giant',
-    correct: 'c'
-  }
-]
+    a: "Gowther",
+    b: "Rasuto",
+    c: "Ritha",
+    d: "Armor Giant",
+    correct: "c",
+  },
+];
 
-const quiz = document.getElementById('quiz')
-const answerEls = document.querySelectorAll('.answer')
-const questionEl = document.getElementById('question')
-const a_text = document.getElementById('a_text')
-const b_text = document.getElementById('b_text')
-const c_text = document.getElementById('c_text')
-const d_text = document.getElementById('d_text')
-const submitBtn = document.getElementById('submit')
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+const sound = document.getElementById("coinSound");
 
-let currentQuiz = 0
-let score = 0
+let currentQuiz = 0;
+let score = 0;
+let coins = 0;
 
-loadQuiz()
+loadQuiz();
 
 function loadQuiz() {
-  deselectAnswers()
-  const currentQuizData = quizData[currentQuiz]
+  deselectAnswers();
+  const currentQuizData = quizData[currentQuiz];
 
-  questionEl.innerText = currentQuizData.question
-  a_text.innerText = currentQuizData.a
-  b_text.innerText = currentQuizData.b
-  c_text.innerText = currentQuizData.c
-  d_text.innerText = currentQuizData.d
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
 }
 
 function getSelected() {
-  let answer = undefined
+  let answer = undefined;
 
-  answerEls.forEach(answerEl => {
+  answerEls.forEach((answerEl) => {
     if (answerEl.checked) {
-      answer = answerEl.id
+      answer = answerEl.id;
     }
-  })
-  return answer
+  });
+  return answer;
 }
 
 function deselectAnswers() {
-  answerEls.forEach(answerEl => {
-    answerEl.checked = false
-  })
+  answerEls.forEach((answerEl) => {
+    answerEl.checked = false;
+  });
 }
 
-submitBtn.addEventListener('click', () => {
+function coinSound(){  
+  sound.play();
+}
+
+submitBtn.addEventListener("click", () => {
   // check to see the answer
-  const answer = getSelected()
+  const answer = getSelected();
 
   if (answer) {
     if (answer === quizData[currentQuiz].correct) {
-      score++
+      // Total score questions
+      score++;
+
+      // insert 10 coins per correct answer
+      coins += 10;
+      document.getElementById("coinsResult").innerHTML = + coins;
+      coinSound();
     }
 
-    currentQuiz++
+    currentQuiz++;
     if (currentQuiz < quizData.length) {
-      loadQuiz()
+      loadQuiz();
     } else {
-      quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions.</h2> <button onclick="location.reload()">Try again?</button>`
+      quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions.</h2> <button onclick="location.reload()">Try again?</button>`;
     }
   }
-})
+});
 
-// Play/pause Music
-
-// Grab ID of audio player
-const Audio = document.getElementById('audio');
-
-// Grab ID of play button
-const playBtn = document.getElementById('play');
-
-// Grab ID of pause button
-const pauseBtn = document.getElementById('pause');
+// Audio
+const Audio = document.getElementById("audio");
+const playBtn = document.getElementById("play");
+const pauseBtn = document.getElementById("pause");
 
 // Play audio & show pause btn
-const playShow = function() {
+const playShow = function () {
   Audio.play();
   playBtn.style.display = "none";
   pauseBtn.style.display = "inline-block";
 };
 
 // Pause audio & show play btn
-const pauseShow = function() {
+const pauseShow = function () {
   Audio.pause();
   playBtn.style.display = "inline-block";
   pauseBtn.style.display = "none";
